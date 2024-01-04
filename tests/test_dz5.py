ccaa33f4c5704ed8b35bf7b502b0c91d9b4adba6
tamayotas <tamaevs@gmail.com>
2024-01-04 17:36:05 +0300

    Создание проекта в Пайчарм Заполнение фалов Дз 5 урок Внесены изменения
    Удалён файл main.py
    изменены названия папки test  и файла conftest
    
    Добавлена папака ресурсы
    Исправлено название onftest

--- tests/test_dz5.py
+++ tests/test_dz5.py
@@ -2,9 +2,8 @@ from selene import browser, have, be, by
 import os.path
 
 
-def test_reg_from():
+def test_reg_form():
     browser.open('/automation-practice-form')
-
     browser.element('.pattern-backgound').should(have.exact_text('Practice Form')).click()
 
     browser.element('#firstName').should(be.blank).type('Ivan')
@@ -14,22 +13,19 @@ def test_reg_from():
     browser.element('#userNumber').should(be.blank).type('9042223322')
 
     browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element(by.text('2000')).click()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element(by.text('October')).click()
-    browser.element('.react-datepicker__day--001').click()
+    browser.element('option:nth-child(7)').click()
+    browser.element('option:nth-child(94)').click()
+    browser.element('div.react-datepicker__day--018').click()
 
-    browser.element('#subjectsContainer').click()
     browser.element('#subjectsInput').type('Maths').press_enter()
-    browser.element('#subjectsInput').type('Chemistry').press_enter()
+    browser.element('#currentAddress').type('Moscow, 123')
+    browser.element('[for="hobbies-checkbox-2"]').click()
+    browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
+    browser.element('#react-select-4-input').type('Merrut').press_enter()
 
     browser.element('#uploadPicture').send_keys(os.path.abspath('picture1.jpeg'))
-
-    browser.element('#currentAddress').should(be.blank).type('Moscow, 123')
-    browser.element('#react-select-3-input').type('Kaluga').press_enter()
-    browser.element('#react-select-4-input').type('Omsk').press_enter()
     browser.element('#submit').press_enter()
+
 
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
     browser.all('.table td:nth-child(2)').should(
@@ -39,12 +35,11 @@ def test_reg_from():
             'Female', '9042223322',
             '01 October,2000',
             'Math',
-            'Chemistry',
+            'Reading',
             'picture1.jpeg',
             'Moscow, 123',
-            'Kaluga Omsk'
+            'Uttar Pradesh Merrut'
         )
     )
 
     browser.element('#closeLargeModal').press_enter()
-    # изменения

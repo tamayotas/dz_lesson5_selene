d68455927c986bd3730d5db9e4eea40eb3d30f25
tamayotas <tamaevs@gmail.com>
2024-01-05 10:28:28 +0300

    Создание проекта в Пайчарм Заполнение фалов Дз 5 урок Внесены изменения
    Удалён файл main.py
    изменены названия папки test  и файла conftest
    
    Добавлена папака ресурсы
    Исправлено название onftest
    
    Внесены исправления, добавлен файл jpeg с папкой ресурсы
    
    Замержен файл картинка
    
    Внесены исправления

--- tests/test_dz5.py
+++ tests/test_dz5.py
@@ -1,32 +1,28 @@
 from selene import browser, have, be, by
-import os.path
+from os.path import dirname, abspath
+import os
 
 
 def test_reg_form():
     browser.open('/automation-practice-form')
     browser.element('.pattern-backgound').should(have.exact_text('Practice Form')).click()
-
     browser.element('#firstName').should(be.blank).type('Ivan')
     browser.element('#lastName').should(be.blank).type('Petrov')
     browser.element('#userEmail').should(be.blank).type('ivan@petrov.com')
     browser.element('label[for="gender-radio-2"]').click()
     browser.element('#userNumber').should(be.blank).type('9042223322')
-
     browser.element('#dateOfBirthInput').click()
-    browser.element('option:nth-child(7)').click()
-    browser.element('option:nth-child(94)').click()
-    browser.element('div.react-datepicker__day--018').click()
-
+    browser.element('.react-datepicker__year-select').element('[value="2000"]').click()
+    browser.element('.react-datepicker__month-select').element('[value="9"]').click()
+    browser.element('.react-datepicker__day--001').click()
     browser.element('#subjectsInput').type('Maths').press_enter()
     browser.element('#currentAddress').type('Moscow, 123')
     browser.element('[for="hobbies-checkbox-2"]').click()
     browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
     browser.element('#react-select-4-input').type('Merrut').press_enter()
-
-    browser.element('#uploadPicture').send_keys(os.path.abspath('../resources/picture1.jpeg'))
+    path = os.path.join(dirname(dirname(abspath(__file__))), "resources")
+    browser.element('#uploadPicture').send_keys(path + '/picture1.jpeg')
     browser.element('#submit').press_enter()
-
-
     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
     browser.all('.table td:nth-child(2)').should(
         have.texts(
